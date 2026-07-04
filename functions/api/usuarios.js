4c4e1c983fc41bac6b2a93733c9e8785438af52d
Patricio Fernandez <pato17fernandez@gmail.com>
2026-07-04 00:45:42 -0400

    Agregar listado de usuarios

--- functions/api/usuarios.js
+++ functions/api/usuarios.js
@@ -1,17 +1,3 @@
-export async function onRequestGet(context) {
-  try {
-    const { results } = await context.env.DB.prepare(`
-      SELECT id, nombre, usuario, rol
-      FROM usuarios
-      ORDER BY id DESC
-    `).all();
-
-    return Response.json(results);
-  } catch (error) {
-    return Response.json({ ok: false, error: error.message }, { status: 500 });
-  }
-}
-
 export async function onRequestPost(context) {
   try {
     const body = await context.request.json();
